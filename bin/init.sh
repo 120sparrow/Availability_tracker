72075657158e94abe50711da3909a5f41e364bd9
Eugene Buranov <120sparrow@gmail.com>
2020-09-10 15:14:40 +0300

    add .gitignore and init.sh

---
+++ bin/init.sh
@@ -0,0 +1,5 @@
+git clone -b master git@github.com:120sparrow/Availability_tracker-backend.git backend
+(cd backend && git checkout master)
+
+git clone -b master git@github.com:120sparrow/Availability_tracker-frontend.git frontend
+(cd frontend && git checkout master)
