0bca328554bb58f5a8d5d1903330024ec7b261cf
Eugene Buranov <120sparrow@gmail.com>
2020-09-10 20:51:13 +0300

    change init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -3,3 +3,9 @@ git clone -b master git@github.com:120sparrow/Availability_tracker-backend.git b
 
 git clone -b master git@github.com:120sparrow/Availability_tracker-frontend.git frontend
 (cd frontend && git checkout master)
+
+docker-compose run --rm backend npx sequelize-cli db:migrate
+
+docker-compose run --rm backend npx sequelize-cli db:seed:all
+
+docker-compose build
